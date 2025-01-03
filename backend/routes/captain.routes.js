3d874ba2cf98407cbd791acacb0d13955200a9ce
Arpit9437 <arpitkaple13@gmail.com>
2025-01-03 20:19:24 +0530

    deployment bug fixes

--- backend/routes/captain.routes.js
+++ backend/routes/captain.routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { body } = require("express-validator");
 const captainController = require("../controllers/captain.controller");
-const authMiddleware = require("../middleware/auth.Middleware");
+const authMiddleware = require("../middleware/auth.middleware");
 const router = express.Router();
 
 router.post('/register', [
