9410c2d2b553cf5ce639950835c1b55945ca11c1
Arpit9437 <arpitkaple13@gmail.com>
2025-01-03 20:12:26 +0530

    deploy related changes - 2

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,31 @@
+const http = require('http');
 const express = require('express');
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const userRoutes = require('./routes/user.routes');
 const captainRoutes = require('./routes/captain.routes');
+const connectToDb = require('./db/db');
+
 dotenv.config();
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.use(cookieParser())
-
+app.use(cookieParser());
 
 app.use('/users', userRoutes);
 app.use('/captains', captainRoutes);
 
-module.exports = app; 
+const port = process.env.PORT || 4000;
+
+const server = http.createServer(app);
+connectToDb();
+
+server.listen(port, () => {
+    console.log(`server running on ${port}`);
+});
+
+module.exports = app;
+
